ca362ef21cbbf1159030f95ff2ce4ee61a62f16a
AmalMansour19 <elhayaa28@gmail.com>
2026-07-04 19:55:55 +0300

    Fix dashboard and recent orders conflicts

---
+++ src/features/dashboard/components/RecentOrder/RecentOrders.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import RecentOrderItem from "./RecentOrderItem";
+import { getDashboard } from "../../services/dashboardApi";
+
+const RecentOrder = ({ dashboard })=>{
+
+    const orders = dashboard?.recentOrders || [];
+    return(
+        <>
+            <section className="rounded-2xl border border-gray-200 bg-white py-3 px-6 shadow-lg w-full min-h-[517px] shadow-lg">
+                <div className=" flex justify-between items-center ">
+                    <div>
+                        <h5 className="md:tracking-[6px] uppercase text-cyan-400 mt-2 md:text-xl text-sm ">Recent orders</h5>
+                        <h2 className=" text-sm md:text-xl mb-4 mt-2 font-semibold">Latest customer activity</h2>
+                    </div>
+                    <p className="bg-slate-200 text-cyan-600  rounded-full md:px-4  py-1 px-2 md:text-sm text-xs">{orders.length} order</p>
+              
+                </div>
+                
+
+                {
+                    orders.map((order) => {
+                        return(
+                            <RecentOrderItem key={order._id} order={order} /> 
+
+                        )
+                        
+                    })
+                }
+                
+
+           </section>
+        
+        </>
+
+    )
+
+
+}
+
+export default RecentOrder;
--- src/features/dashboard/pages/Dashboard.jsx
+++ src/features/dashboard/pages/Dashboard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { getDashboard } from "../services/dashboardApi";
+import TopProduct from "../components/TopProduct/TopProduct";
+// import OrderStatus from '../components/OrderStatus';
+import RecentOrders from "../components/RecentOrder/RecentOrders";
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard Dashboard</h1>
+
+   
+
+    <div className="p-4 md:p-8 ">
+      <div className="grid grid-cols-1 lg:grid-cols-[1.4fr_1fr] gap-8 ">
+        {/* <OrderStatus /> */}
+        <TopProduct dashboard={dashboard} />
+      </div>
+
+      <div className="mt-6">
+        <RecentOrders dashboard={dashboard} />
+      </div>
+
     </div>
   );
 }
