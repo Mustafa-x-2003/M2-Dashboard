2d13e716ea53a4c63d031b8ce002ded003946f78
AmalMansour19 <elhayaa28@gmail.com>
2026-07-05 09:23:58 +0300

    fix(DASH-006): remove top products from dashboard

--- src/features/dashboard/pages/Dashboard.jsx
+++ src/features/dashboard/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import React, { useEffect, useState } from "react";
 import { getDashboard } from "../services/dashboardApi";
-import TopProduct from "../components/TopProduct/TopProduct";
+// import TopProduct from "../components/TopProduct/TopProduct";
 // import OrderStatus from '../components/OrderStatus';
 import RecentOrders from "../components/RecentOrder/RecentOrders";
 function Dashboard() {
-  
+
   const [dashboard, setDashboard] = useState(null);
 
   useEffect(() => {
@@ -22,7 +22,7 @@ function Dashboard() {
     <div className="p-4 md:p-8 ">
       <div className="grid grid-cols-1 lg:grid-cols-[1.4fr_1fr] gap-8 ">
         {/* <OrderStatus /> */}
-        <TopProduct dashboard={dashboard} />
+        {/* <TopProduct dashboard={dashboard} /> */}
       </div>
 
       <div className="mt-6">
