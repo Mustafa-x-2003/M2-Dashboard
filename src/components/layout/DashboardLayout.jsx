5e8648b6282628f11d0f251c80a73a04398aaf1e
faty-ka <103934399+faty-ka@users.noreply.github.com>
2026-07-06 01:05:29 +0300

    Implement responsive orders page

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -3,20 +3,22 @@ import { Outlet } from "react-router";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
+
 export default function DashboardLayout() {
   return (
     <div className="flex h-screen">
-      {/* Sidebar */}
-      <aside className="w-80 bg-gray-600">
+      <aside className="hidden lg:block lg:w-80 bg-gray-600">
         <Sidebar />
       </aside>
 
-      {/* Content */}
-      <div className="flex flex-1 flex-col">
+      <div className="flex min-w-0 flex-1 flex-col">
         <Navbar />
 
-        <main className="flex-1 bg-amber-600 flex justify-center items-center">
-          <Outlet />
+        <main className="min-w-0 flex-1 overflow-y-auto bg-amber-600">
+          <div className="min-h-full">
+            <Outlet />
+            
+          </div>
         </main>
       </div>
     </div>
