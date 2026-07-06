28369f1bd48877a0af10218e2ddb1baffbda826f
karoleenmaged <karoleenmaged3@gmail.com>
2026-07-06 06:47:56 +0300

    Build Dashboard Statistics Card

--- src/features/dashboard/components/StatsCard.jsx
+++ src/features/dashboard/components/StatsCard.jsx
@@ -1,5 +1,4 @@
 import {React,useState,useEffect} from "react";
-import axiosInstance from "../../../services/api/axios";
 const StatsCard=({title,icon,data,color,status})=>{
     return (
         <div className="bg-white border-slate-200/70 dark:bg-slate-900 dark:border-slate-800 rounded-2xl px-5 pt-6 relative overflow-hidden h-[220px] hover:shadow-lg hover:shadow-slate-400 hover:-translate-y-2 transition-all duration-500">
@@ -10,7 +9,7 @@ const StatsCard=({title,icon,data,color,status})=>{
                 <h2 className="text-3xl mt-3 font-bold">{data}</h2>
                 <h2 className="text-sm text-slate-400 font-light mt-3">{status}</h2>
             </div>
-            <div className={`${color} p-4 rounded-2xl justify-center h-full mt-3`}>
+            <div className={`${color} p-4 rounded-2xl justify-center h-full mt-3 transition-transform duration-300 hover:rotate-12`}>
                 {icon}
             </div>
             </div>
