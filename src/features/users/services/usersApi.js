8d449b64dc4ed0475bebd29e142f1a6747568c49
Nagham <mostafaa01094803310@gmail.com>
2026-07-08 22:54:18 +0300

    fix: resolve users and products API issues

--- src/features/users/services/usersApi.js
+++ src/features/users/services/usersApi.js
@@ -2,27 +2,28 @@ import axiosInstance from "../../../services/api/axios";
 import { ENDPOINTS } from "../../../services/endpoints";
 
 export const getUsers = async () => {
-    const response = await axiosInstance.get(ENDPOINTS.USERS);
-
-    return response.data;
+  const response = await axiosInstance.get(ENDPOINTS.USERS.ALL);
+  return response.data;
 };
 
 export const updateUser = async (id, userData) => {
-    const response = await axiosInstance.patch(
-        ENDPOINTS.USER_BY_ID(id),
-        userData
-    );
-    return response.data;
+  const response = await axiosInstance.patch(
+    ENDPOINTS.USERS.BY_ID(id),
+    userData
+  );
+
+  return response.data;
 };
 
 export const toggleUserRole = async (id, currentRole) => {
-  const newRole = currentRole === "admin"
-    ? "customer"
-    : "admin";
+  const newRole = currentRole === "admin" ? "customer" : "admin";
 
   const response = await axiosInstance.patch(
-    ENDPOINTS.USER_BY_ID(id),
-      {role: newRole,}
+    ENDPOINTS.AUTH.CHANGE_ROLE,
+    {
+      userId: id,
+      role: newRole,
+    }
   );
 
   return response.data;
@@ -30,7 +31,7 @@ export const toggleUserRole = async (id, currentRole) => {
 
 export const deleteUser = async (id) => {
   const response = await axiosInstance.delete(
-    ENDPOINTS.USER_BY_ID(id)
+    ENDPOINTS.USERS.BY_ID(id)
   );
 
   return response.data;
